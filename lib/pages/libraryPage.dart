e45a2ff120a51334102f406dbcb1e02fa16b5c64
process4 <info@process4.biz>
2019-03-15 16:21:23 +0100

    jsons for /library added

--- lib/pages/libraryPage.dart
+++ lib/pages/libraryPage.dart
@@ -2,16 +2,19 @@ import 'package:falavastr/drawer.dart';
 import 'package:flutter/material.dart';
 
 class LibraryPage extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       drawer: DrawerOnly(),
       appBar: AppBar(
-        title: Text("Настройки"),
+        title: Text("Библиотека"),
       ),
-      body: Center(
-        child: Text("Здесь будет Библиотека")
-      ),
+      body: Expanded(child: ListView(
+        children: <Widget>[
+          
+        ],
+      ),),
     );
   }
 }
